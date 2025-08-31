7dddafcb2a776f3c0e29cf8e253099640ce81c89
TyphoonDorothy <odarkaz66@gmail.com>
2025-08-31 15:21:42 +0300

    Made table forecast

--- main.py
+++ main.py
@@ -2,23 +2,42 @@ import requests
 import pandas as pd
 
 api_key = 'a6255d0f011142e8b8c104239252908'
+pd.set_option("display.max_columns", None)
 
 def tomorrow_forecast(cities):
+    forecast = {'max temperature': [], 'min temperature': [], 'average humidity': [],
+                'max wind speed': [], 'wind direction': []}
+
     for city in cities:
-        response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=2&aqi=no&alerts=no')
-        data = response.json()
-        maxtemp_c = data['forecast']['forecastday'][1]['day']['maxtemp_c']
-        mintemp_c = data['forecast']['forecastday'][1]['day']['mintemp_c']
-        avghumidity = data['forecast']['forecastday'][1]['day']['avghumidity']
-        maxwind_kph = data['forecast']['forecastday'][1]['day']['maxwind_kph']
+        try:
+            response = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=2&aqi=no&alerts=no')
+            data = response.json()
+            data = data['forecast']['forecastday'][1]
 
-        wind_dir = 'Uknown'
-        hours = data['forecast']['forecastday'][1]['hour']
-        for hour in range(0, 24):
-            if hours[hour]['wind_kph'] == maxwind_kph:
-                wind_dir = hours[hour]['wind_dir']
-            else:
-                continue
-        print(maxtemp_c, mintemp_c, avghumidity, maxwind_kph, wind_dir)
+            maxtemp_c = data['day']['maxtemp_c']
+            mintemp_c = data['day']['mintemp_c']
+            avghumidity = data['day']['avghumidity']
+            maxwind_kph = data['day']['maxwind_kph']
+            wind_dir = 'Unknown'
 
-tomorrow_forecast(['Kyiv', 'Madrid'])
+            for hour in range(0, 24):
+                if data['hour'][hour]['wind_kph'] == maxwind_kph:
+                    wind_dir = data['hour'][hour]['wind_dir']
+                else:
+                    continue
+
+            forecast['max temperature'].append(maxtemp_c)
+            forecast['min temperature'].append(mintemp_c)
+            forecast['average humidity'].append(avghumidity)
+            forecast['max wind speed'].append(maxwind_kph)
+            forecast['wind direction'].append(wind_dir)
+
+        except Exception as e:
+            cities.remove(city)
+            print(f'Unable to load {city} for reason {e}')
+
+    df = pd.DataFrame(data=forecast, index=cities)
+    print(df )
+
+
+tomorrow_forecast(['Kyiv', 'Madrid', 'Chisinau', 'Amsterdam'])
